c6120f05643a92c71ff1bdd55f8451575c646205
Tjeri <tjeri92@gmail.com>
2024-07-04 21:49:57 +0200

    implementation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TelegramAdminPingBot
 Notify Admins, if a user uses @admin
+
+## Environment
+Needed Variables in Environment
+- `BOT_TOKEN`: Telegram API Token
+- `OVERWATCH_CHANNEL`: Channel ID if the channel, where @admin should be used
+- `ADMIN_CHANNEL`: Admin Channel, that is notified
+- `NOTIFY_MESSAGE`: Message posted in Admin Channel
+- `NOTIFY_REPLY`: Answer sent to @admin Message
---
+++ bot.py
@@ -0,0 +1,29 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = getenv('BOT_TOKEN')
+OVERWATCH_CHANNEL = int(getenv('OVERWATCH_CHANNEL'))
+ADMIN_CHANNEL = int(getenv('ADMIN_CHANNEL'))
+NOTIFY_MESSAGE = getenv('NOTIFY_MESSAGE')
+NOTIFY_REPLY = getenv('NOTIFY_REPLY')
+
+from telegram import Update
+from telegram.ext import Application, ContextTypes, MessageHandler, filters
+
+
+async def check_ping(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    if update.message.chat.id == OVERWATCH_CHANNEL and '@admin' in update.message.text:
+        await context.bot.send_message(chat_id=ADMIN_CHANNEL, text=NOTIFY_MESSAGE)
+        await update.message.reply_text(NOTIFY_REPLY)
+
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND & filters.ChatType.GROUPS, check_ping))
+    application.run_polling(allowed_updates=Update.MESSAGE)
+
+
+if __name__ == '__main__':
+    main()
